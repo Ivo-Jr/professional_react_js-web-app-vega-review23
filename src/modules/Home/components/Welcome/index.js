04f09ea825544e583852fbd8495788dfe1ae3221
rafael <rafaelmdeveloper@gmail.com>
2023-06-26 04:37:51 -0300

    #448

--- src/modules/Home/components/Welcome/index.js
+++ src/modules/Home/components/Welcome/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 import {
   Container,
@@ -14,6 +15,16 @@ import { FiArrowUpRight } from "react-icons/fi";
 import planetImg from '../../assets/planet.png'
 
 export const Welcome = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleCTAClick = () => {
+    setScrollPosition(scrollPosition + 600);  // 20cm correspondem a aproximadamente 200 pixels
+    scroll.scrollTo(scrollPosition, {
+      duration: 1000,  // Define a duração da rolagem para 1000 milissegundos (1 segundo)
+      smooth: "easeInOutQuint"  // Define o tipo de transição para uma função de aceleração suave
+    });
+  }
+
   return (
     <Container id="home">
       <Content imgprops={planetImg}>
@@ -25,7 +36,7 @@ export const Welcome = () => {
         Revolucionamos a sua jornada para a Indústria 4.0. Com soluções de automação, digitalização, Internet das Coisas, Computação em Nuvem e Inteligência Artificial, remodelamos seus processos existentes para reduzir custos, eliminar desperdícios e levar sua empresa a novos patamares de eficiência.
         </SubTitle>
 
-        <CTAButton>
+        <CTAButton onClick={handleCTAClick}>
           Eu quero conhecer
           <FiArrowUpRight color="#fff" />
         </CTAButton>
